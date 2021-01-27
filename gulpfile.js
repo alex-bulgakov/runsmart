95fb3ac2b272f007f22ca9ecfa56332e9895ca50
alebul <master8423@gmail.com>
2021-01-27 09:40:34 +0300

    change sass folder

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,7 @@ gulp.task('server', function() {
 gulp.task('styles', function() {
   return gulp.src("src/sass/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({
-          prefix: "",
-          suffix: ".min"
-        }))
+        .pipe(rename("style.min.css"))
         .pipe(autoprefixer({
           cascade: false
         }))
