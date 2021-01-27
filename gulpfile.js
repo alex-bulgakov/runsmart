e00ae5967be3fd57555541d50ab3d8c5b190d7a7
alebul <master8423@gmail.com>
2021-01-27 10:00:09 +0300

    edit sass in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-  return gulp.src("src/sass/*.+(scss|sass)")
+  return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename("style.min.css"))
         .pipe(autoprefixer({
