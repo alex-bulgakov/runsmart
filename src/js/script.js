2ebe02898bb9182a7dd9a11e2a6ae936c5f2fca0
alebul <master8423@gmail.com>
2021-02-04 16:49:17 +0300

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,10 @@
-let number = 7;
-console.log(number);
+//slick slider
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true
+  });
+});
